4d81b91e3b4d2d9c1a475f5be79f3317027e23bd
Kallirroi <kallirroi.retzepi@gmail.com>
2020-03-05 13:08:38 -0500

    have a grid with an underlying guide, have to figure out how to move things freely

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import useDraggableItems from 'react-drag-elements'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const initialItems = [
+  { id: 1}, // colored
+  { id: 2},
+  { id: 3},
+  { id: 4},
+  { id: 5},
+  { id: 6},
+  { id: 7},
+]
 
-export default App;
+export default function App() {
+  const { items, getItemProps } = useDraggableItems({
+    initialItems, 
+    delay: 10, 
+    debounceMs: 10, 
+    easeFunction: 'cubic-bezier(.39, .28, .13, 1.14)',
+  })
+
+  // Fade in demo
+  React.useEffect(() => {
+    document.body.style.opacity = '1'
+  }, [])
+
+  const classNames = [
+    'fullWidth',
+    'twothirds',
+    'twothirds',
+    'circle',
+    'halfGridHeight',
+    'fifthGridHeight',
+    'fifthGridHeight',
+  ]
+
+  return (
+    <div className="main">
+      <span className="shadowMain"> 
+        <span className="item"></span>
+        <span className="item"></span>
+        <span className="item"></span>
+        <span className="item"></span>
+        <span className="item"></span>
+        <span className="item"></span>
+        <span className="item"></span>
+        <span className="item"></span>
+        <span className="item"></span>
+      </span>
+      <div className='draggingArea'>
+        {items.map((item) => (
+          <li key={item.id} className={classNames[item.id-1]} {...getItemProps(item.id)}> </li>
+        ))}
+      </div>
+    </div>
+  )
+}
