1183bbe35405ec772e505fda217e5b9e1920db27
Kallirroi <kallirroi.retzepi@gmail.com>
2020-03-12 19:09:58 -0400

    simplified text

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -6,34 +6,22 @@ function Information() {
 	const { Panel } = Collapse;
 	return (
 		<div className='Information'>
-			<h1> Grid Systems</h1>
-			<h3> An interactive supplement to <a href="https://www.goodreads.com/book/show/350964.Grid_Systems"> a wonderful book </a> by Kimberly Elam.</h3>
-			<p>*better viewed on Chrome</p>
+			<h1> Grids playground</h1>
+			<h3> An interactive supplement to the book <a href="https://www.goodreads.com/book/show/350964.Grid_Systems"> Grid Systems</a> by Kimberly Elam.</h3>
+			<p><em>*better viewed on Chrome.</em></p>
 			<Collapse>
-			  	<Panel showArrow={false} header="WHAT IS THIS?" key="0" className="site-collapse-custom-panel">
-			      	From the book: <em><p>A 3x3 structure is the format for exploring texture and composition. This simple grid system provides a wide range of variation for exploration within a controlled system of organization. Six rectangles are the compositional elements. A small circle is also used to provide an element for balance, visual control of the composition and contrast.</p> 
-					</em>
-					<p> I built this interactive prototype in order to rapidly create thumbnails by arranging the compositional elements in accordance to the exercises below.</p>
-					<p> Have fun! </p>
+			  	<Panel showArrow={false} header="WHAT IS THIS?" key="1" className="site-collapse-custom-panel">
+			  		<p>This is an interactive prototype for rapid grid design explorations.</p>
+			      	<p>From Kimberly Elam's book:</p>  <em>A 3x3 structure is the format for exploring texture and composition. This simple grid system provides a wide range of variation for exploration within a controlled system of organization. Six rectangles are the compositional elements. A small circle is also used to provide an element for balance, visual control of the composition and contrast.</em>
 			    </Panel>
-			    <hr/>
-			    <Panel showArrow={false} header="1. HORIZONTAL COMPOSITION" key="1" className="site-collapse-custom-panel">
-			      <p>Here are three variations you can explore:
-					<li>- Long rectangle in top position</li>
-					<li>- Long rectangle in bottom position</li>
-					<li>- Long rectangle in interior position</li>
-				</p>
+
+			  	<Panel showArrow={false} header="IDEAS" key="2" className="site-collapse-custom-panel">
+			    	<p>Here are some variations you can explore, starting by focusing on the placement of the long rectangle. </p>
+					<li>• Long rectangle in top position</li>
+					<li>• Long rectangle in bottom position</li>
+					<li>• Long rectangle in interior position</li>
 			    </Panel>
-			    <hr/>
-			    <Panel showArrow={false} header="2. HORIZONTAL/VERTICAL COMPOSITION" key="2" className="site-collapse-custom-panel">
-			      <p>Coming soon </p>
-			    </Panel>
-			    <hr/>
-			    <Panel showArrow={false} header="3. DIAGONAL COMPOSITION" key="3" className="site-collapse-custom-panel">
-			      <p>Coming soon </p>
-			    </Panel>
-			    <hr/>
-			  </Collapse>
+			</Collapse>
 
 			<p className='credits'>Made by <a href="https://kalli-retzepi.com/" target='_blank' rel="noopener noreferrer" >Kalli</a> during her time at the <a href="https://www.recurse.com/" target='_blank' rel="noopener noreferrer">Recurse Center</a>.</p>
 		</div>
